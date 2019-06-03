1dafa819365e9788ea4ef629acce152804fcbc8a
NapoleonDLP <napoleon.dlp@gmail.com>
2019-06-03 12:55:32 -0700

    Clean up code and delete ghost comments

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,27 +1,15 @@
 var ForagerBee = function() {
-// call the Bee superclass
 Bee.call(this);
-// an age property that is set to 10
 this.age = 10;
-// a job property that is set to find pollen
 this.job = 'find pollen';
-// a color property inherited from bee that is set to yellow
-  //inherits from bee no need to adjust
-// a food property that is inherited from grub
-  //inherits from bee no need to adjust
-// an eat method that is inherited from grub
-  //inherits from bee no need to adjust
-// a canFly property that is set true
 this.canFly = true;
-// a treasureChest property that is set to an empty array []
 this.treasureChest = [];
 };
 
-// set the prototype
 ForagerBee.prototype = Object.create(Bee.prototype);
-// a forage method that allows the bee to add a treasure to the treasureChest
+
 ForagerBee.prototype.forage = function(treasure) {
   this.treasureChest.push(treasure);
-}
-// set the constructor
+};
+
 ForagerBee.prototype.constructor = ForagerBee;
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,29 +1,18 @@
 var HoneyMakerBee = function() {
-  // call the Bee superclass
   Bee.call(this);
-  // an age property that is set to 10
   this.age = 10;
-  // a job property that is set to make honey
   this.job = 'make honey';
-  // a color property inherited from bee that is set to yellow
-    //inherited from bee so no need to adjust here
-  // a food property that is inherited from grub
-    //inherited from grub which was not changed at bee
-  // an eat method that is inherited from grub
-    //inherited from grub which was not changed at bee
-  // a honeyPot property that is set to 0
   this.honeyPot = 0;
 };
 
-// set the prototype
 HoneyMakerBee.prototype = Object.create(Bee.prototype);
-// a makeHoney method that adds 1 to that honeyBee\'s honeyPot
+
 HoneyMakerBee.prototype.makeHoney = function() {
   this.honeyPot++;
-}
-// a giveHoney method that subtracts 1 from that honeyBee\'s honeyPot
+};
+
 HoneyMakerBee.prototype.giveHoney = function() {
   this.honeyPot--;
-}
-// set the constructor
+};
+
 HoneyMakerBee.prototype.constructor = HoneyMakerBee;
