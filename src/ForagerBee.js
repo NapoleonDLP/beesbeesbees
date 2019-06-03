5b3dde5b6fafd81083368b3c90147995f1c37614
NapoleonDLP <napoleon.dlp@gmail.com>
2019-06-03 12:38:00 -0700

    Creates implementation plan for ForagerBee

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,14 @@
 var ForagerBee = function() {
+// call the Bee superclass
+// an age property that is set to 10
+// a job property that is set to find pollen
+// a color property inherited from bee that is set to yellow
+// a food property that is inherited from grub
+// an eat method that is inherited from grub
+// a canFly property that is set true
+// a treasureChest property that is set to an empty array []
 };
 
+// set the prototype
+// a forage method that allows the bee to add a treasure to the treasureChest
+// set the constructor
