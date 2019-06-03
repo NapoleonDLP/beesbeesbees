2afbbcbe4f5999560c7680844e4e43ba1700e36b
NapoleonDLP <napoleon.dlp@gmail.com>
2019-06-03 11:52:48 -0700

    Completed pseudo code for Bee

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,11 @@
 var Bee = function() {
+//call the Grub superclass
+//an age property that is set to 5
+//a color property that is set to yellow
+//a food property that is inherited from grub
+//an eat method that is inherited from grub
+//a job property that is set to keep on growing
 };
 
+//set the prototype
+//set the constructor
