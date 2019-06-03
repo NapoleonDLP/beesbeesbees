c2c44ad14b9441e099c7abc1608a0f955686d82e
NapoleonDLP <napoleon.dlp@gmail.com>
2019-06-03 12:36:13 -0700

    Complete HoneyMakerBee

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,15 +1,29 @@
 var HoneyMakerBee = function() {
-  // an age property that is set to 10
-  // a job property that is set to make honey
-  // a color property inherited from bee that is set to yellow
-  // a food property that is inherited from grub
-  // an eat method that is inherited from grub
-  // a honeyPot property that is set to 0
-  // a makeHoney method that adds 1 to that honeyBee\'s honeyPot
-  // a giveHoney method that subtracts 1 from that honeyBee\'s honeyPot
   // call the Bee superclass
+  Bee.call(this);
+  // an age property that is set to 10
+  this.age = 10;
+  // a job property that is set to make honey
+  this.job = 'make honey';
+  // a color property inherited from bee that is set to yellow
+    //inherited from bee so no need to adjust here
+  // a food property that is inherited from grub
+    //inherited from grub which was not changed at bee
+  // an eat method that is inherited from grub
+    //inherited from grub which was not changed at bee
+  // a honeyPot property that is set to 0
+  this.honeyPot = 0;
 };
 
 // set the prototype
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+// a makeHoney method that adds 1 to that honeyBee\'s honeyPot
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot++;
+}
+// a giveHoney method that subtracts 1 from that honeyBee\'s honeyPot
+HoneyMakerBee.prototype.giveHoney = function() {
+  this.honeyPot--;
+}
 // set the constructor
-
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
